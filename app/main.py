5ffeeaf9f36d16f575726cb18af252790949197e
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-01 22:05:47 -0800

    update posts by id

--- app/main.py
+++ app/main.py
@@ -84,12 +84,17 @@ def delete_post(id:int,db:Session=Depends(get_db)):
         detail=f"post with id: {id} does not exist") 
     post.delete(synchronize_session=False)  
     db.commit()  
-    return Response(status_code=status.HTTP_204_NO_CONTENT)    
-@app.put("/posts/{id}")  
-def update_post(id:int,post:Post):
-    cursor.execute("""UPDATE posts SET title=%s,content=%s WHERE id=%sRETURNING *""",(post.title,post.content,(str(id))))  
-    updated_post=cursor.fetchone()
-    conn.commit()
-    if updated_post==None:
-        raise HTTPException(status_code==status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
-    return {"data":updated_post}    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)  
+  
+@app.put("/posts/{id}",status_code=status.HTTP_404_NOT_FOUND)  
+def update_post(id:int,updated_post:Post,db:Session=Depends(get_db)):
+    #cursor.execute("""UPDATE posts SET title=%s,content=%s WHERE id=%sRETURNING *""",(post.title,post.content,(str(id))))  
+    #updated_post=cursor.fetchone()
+    #conn.commit()
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+    post=post_query.first()
+    if post==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id:{id} does not exist")
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit()
+    return {"data":post_query.first()}    
