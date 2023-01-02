826e8f0950a121a6a440c39f5afd4a0aad139bdf
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-01 21:54:01 -0800

    delete posts by id

--- app/main.py
+++ app/main.py
@@ -73,14 +73,17 @@ def get_post(id:int,db:Session=Depends(get_db)):
     return{"post-detail":post}    
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int):
-    cursor.execute("""DELETE FROM posts WHERE id=%s returning *""",(str(id),))  
-    deleted_post=cursor.fetchone() 
-    conn.commit()
+def delete_post(id:int,db:Session=Depends(get_db)):
+    #cursor.execute("""DELETE FROM posts WHERE id=%s returning *""",(str(id),))  
+    #deleted_post=cursor.fetchone() 
+    #conn.commit()
     #index=find_index_post(id)
-    if deleted_post==None:
+    post=db.query(models.Post).filter(models.Post.id==id)
+    if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
         detail=f"post with id: {id} does not exist") 
+    post.delete(synchronize_session=False)  
+    db.commit()  
     return Response(status_code=status.HTTP_204_NO_CONTENT)    
 @app.put("/posts/{id}")  
 def update_post(id:int,post:Post):
