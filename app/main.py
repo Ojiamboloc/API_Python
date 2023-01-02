e41158634707f9202efe8fd8f55ffeab97aeee06
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-01 21:49:18 -0800

    fetch posts by id

--- app/main.py
+++ app/main.py
@@ -53,22 +53,25 @@ def create_posts(post:Post,db:Session=Depends(get_db)):
                   #(post.title,post.content))
     #new_post=cursor.fetchone()
     #conn.commit()
-    new_post=models.Post(title=post.title,content=post.content,published=post.published)
+    
+    new_post=models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return{"data":new_post}
 
 @app.get("/posts/{id}")
-def get_post(id:int):
-    cursor.execute("""SELECT * FROM posts WHERE id=%s""",(str(id)))
-    post=cursor.fetchone()
-    print(post)
+def get_post(id:int,db:Session=Depends(get_db)):
+    #cursor.execute("""SELECT * FROM posts WHERE id=%s""",(str(id)))
+   # post=cursor.fetchone()
+   # print(post)
     #post=find_post(id)
+    post=db.query(models.Post).filter(models.Post.id==id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
         detail=f"post with id:{id} was not found")
     return{"post-detail":post}    
+
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id:int):
     cursor.execute("""DELETE FROM posts WHERE id=%s returning *""",(str(id),))  
